ba25f3d035acefca09fa927386921768765be821
ison <ramonrocharezende@gmail.com>
2021-09-03 09:10:09 -0300

    Adds filter on scripts and get.sh script

--- dec.sh
+++ dec.sh
@@ -2,6 +2,14 @@
 
 read -s -p "Enter master password: " master_pass
 echo ""
-for pass in $(cat passwords.txt);do
-	echo "$(echo $pass | cut -d '#' -f 2): $(echo $pass | cut -d '#' -f 1 | base64 -d -w 0 | openssl enc -d -aes-256-cbc -pbkdf2 -k $master_pass 2>/dev/null)";
-done
+filter=$1
+if ! [[ -z $filter ]]
+then
+	for pass in $(cat passwords.txt | grep $filter);do
+		echo "$(echo $pass | cut -d '#' -f 2): $(echo $pass | cut -d '#' -f 1 | base64 -d -w 0 | openssl enc -d -aes-256-cbc -pbkdf2 -k $master_pass 2>/dev/null)";
+	done
+else
+	for pass in $(cat passwords.txt);do
+		echo "$(echo $pass | cut -d '#' -f 2): $(echo $pass | cut -d '#' -f 1 | base64 -d -w 0 | openssl enc -d -aes-256-cbc -pbkdf2 -k $master_pass 2>/dev/null)";
+	done
+fi
--- keys.sh
+++ keys.sh
@@ -1,3 +1,11 @@
 #! /bin/bash
-
-cat passwords.txt | cut -d '#' -f 2
+echo $1
+filter=$1
+if ! [[ -z $filter ]]
+then
+    for pass in $(cat passwords.txt | grep $filter);do
+        echo $pass | cut -d '#' -f 2
+    done
+else
+    cat passwords.txt | cut -d '#' -f 2
+fi
